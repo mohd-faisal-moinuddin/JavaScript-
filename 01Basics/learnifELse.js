0e4b6c55631a5b7e9dad4abe2531b28cbbe43a22
faisalmoinuddin99 <51179625+faisalmoinuddin99@users.noreply.github.com>
2019-05-30 04:47:07 +0530

    Add files via upload

---
+++ 01Basics/learnifELse.js
@@ -0,0 +1,53 @@
+// let userName = "faisal123";
+// let password = "123";
+
+// if (userName == "faisal123" && password == "125") {
+//   console.log("welcome");
+// } else {
+//   console.log("error");
+// }
+
+let x = 10;
+let y = 20;
+
+if (y > x) {
+  if (x < y) {
+    if (x == 100 && y == 20) {
+      x = 90;
+    }
+  }
+}
+// console.log(x + " " + y);
+
+// if (true) {
+//   console.log("i am inside if block");
+// } else if (true) {
+//   console.log("i am inside else if");
+// } else {
+//   console.log("if all the if fails,then else comes");
+// }
+//  console.log("now i am out");
+
+// var whoIsHere = "abc";
+
+// if (whoIsHere === "user") {
+//   console.log("Greeting message");
+//   console.log("view all courses");
+// } else if (whoIsHere === "teacher") {
+//   console.log("Greeting message");
+//   console.log("view his/her courses");
+// } else {
+//   console.log("Message: Please verify your email");
+// }
+
+let myGrade = 0;
+
+if (myGrade === 10 || myGrade >= 6) {
+  console.log("Amazing");
+} else if (myGrade === 5 || myGrade >= 4) {
+  console.log("Good");
+} else if (myGrade === 3 || myGrade >= 1) {
+  console.log("Poor");
+} else if (myGrade === 0) {
+  console.log("Fail");
+}
